9394c8e16df5fecbf8e6b3e9f544d63cb15b457e
Kiran Shila <me@kiranshila.com>
2021-03-16 18:59:57 -0400

    Some docs

---
+++ README.md
@@ -0,0 +1,4 @@
+# RF-CLJS
+
+[![cljdoc badge](https://cljdoc.org/badge/manifold/manifold)](https://cljdoc.org/d/manifold/manifold/CURRENT)
+Common routines for manipulations of RF/Microwave networks in ClojureScript
--- src/rf_cljs/math/complex.cljs
+++ src/rf_cljs/math/complex.cljs
@@ -2,21 +2,42 @@
   (:require
    ["mathjs" :as mathjs]))
 
-(defn complex [re im]
+(defn complex
+  "Generates a complex number `re + i*im` from `re` and `im`"
+  [re im]
   (mathjs/Complex (str re "+" im "i")))
 
-(defn arg [x]
+(defn argument
+  "Computes the argument of a complex value `x`
+  For complex number `a+bi`, argument is computed as `(atan2 b a)`
+
+  For matrices, the function is evaluted element wise"
+  [x]
   (mathjs/arg x))
 
-(defn conjugate [x]
+(defn conjugate
+  "Computes the complex conjugate of complex value `x`
+  For complex number `a+bi`, the conjugate is computed as `a-bi`
+
+  For matrices, the function is evaluated element wise"
+  [x]
   (mathjs/conj x))
 
-(defn imaginary [x]
+(defn imaginary
+  "Returns the imaginary component of complex value `x`
+
+  For matrices, the function is evaluated element wise"
+  [x]
   (mathjs/im x))
 
-(defn real [x]
+(defn real
+  "Returns the real component of complex value `x`
+
+  For matrices, the function is evaluated element wise"
+  [x]
   (mathjs/re x))
 
 (defn random
+  "Generates a random complex number with optional `max` argument"
   ([] (complex (rand) (rand)))
   ([max] (complex (rand max) (rand max))))
