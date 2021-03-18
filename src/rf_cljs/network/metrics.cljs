3220fd89d2558f0ae3e8db0a65cc1334986a9aae
Kiran Shila <me@kiranshila.com>
2021-03-18 18:21:09 -0400

    Stability measures

--- src/rf_cljs/network/metrics.cljs
+++ src/rf_cljs/network/metrics.cljs
@@ -1,5 +1,8 @@
 (ns rf-cljs.network.metrics
-  (:require [rf-cljs.network.params :as params]))
+  (:refer-clojure :exclude [+ - * /])
+  (:require [rf-cljs.network.params :as params]
+            [rf-cljs.math.matrix :as mat]
+            [rf-cljs.math.operations :refer [abs square + - * /]]))
 
 (defn passivity [])
 
@@ -14,15 +17,30 @@
 
 (defn reciprocal? [])
 
+(defn -per-f [f m]
+  (case (count (mat/shape m))
+    2 (f m)
+    3 (mat/broadcast m f 0)))
+
+(defn -destructure-s [s]
+  [(mat/idx s 0 0) (mat/idx s 0 1)
+   (mat/idx s 1 0) (mat/idx s 1 1)])
+
+(defn delta
+  [s]
+  (assert (params/two-port? s) "S-Parameters must be two port")
+  (-per-f #((let [[S11 S12 S21 S22] (-destructure-s s)]
+              (- (* S11 S22) (* S12 S21))))
+          s))
+
 (defn stab-fact
   "Stability factor (K)"
   [s]
-  (assert (params/two-port? s) "S-Parameters must be two port"))
-
-(defn stab-meas
-  "Stability measure (delta)"
-  [s]
-  (assert (params/two-port? s) "S-Parameters must be two port"))
+  (assert (params/two-port? s) "S-Parameters must be two port")
+  (-per-f #((let [[S11 S12 S21 S22] (-destructure-s s)]
+              (/ (- 1 (square (abs S11)) (square (abs S22)) (square (abs (delta s))))
+                 (* 2 (abs (* S21 S12))))))
+          s))
 
 (defn gmax
   "Maximum available gain"
