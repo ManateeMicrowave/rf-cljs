2e603a152703b6f8de487a10c1ad61d428839e08
Eric Valentino <evalentino@mail.usf.edu>
2021-03-18 15:09:34 -0400

    networks ->network

--- src/rf_cljs/core.cljs
+++ src/rf_cljs/core.cljs
@@ -2,7 +2,7 @@
   (:refer-clojure :exclude [+ * /])
   (:require
    ["mathjs" :as mathjs]
-   [rf-cljs.networks.network :as network]
+   [rf-cljs.network.network :as network]
    [rf-cljs.math.operations :refer [acos cos square + hypot norm log10 abs * /]]
    [rf-cljs.math.complex :as complex]
    [rf-cljs.math.matrix :as mat]))
