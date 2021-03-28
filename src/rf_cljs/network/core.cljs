1f1ab5bda3dbc23b0b1426ee112b1134e5b98363
Eric Valentino <ervalentino135@gmail.com>
2021-03-27 23:29:46 -0400

    Implementing more of linear interpolation function

--- src/rf_cljs/network/core.cljs
+++ src/rf_cljs/network/core.cljs
@@ -6,15 +6,44 @@
             [rf-cljs.math.operations :as op]
             [rf-cljs.math.operations :refer [abs square + - * / sqrt]]))
 
-(defn interp-one-freq-linear
-  [net f]) ; TODO implement
+(defn -interp-to-s-linear
+  [net desired-freq] ; TODO implement 
+  (let [s (:s net)
+        freq (:freq net)
+        n-desired-freqs (count desired-freq)]
+    (loop [i_d 0 ;Index of the desired frequency that we are going to interpolate on
+           i_c 0 ;Index of the `from` frequency range. 
+           ]
+      (if-not (= (inc i_d))
+        (let [f_des (nth i_d desired-freq)
+              f_lo (nth i_c freq)
+              f_hi (nth (inc i_c) freq)]
+          (if-not 
+           (and
+              (op/lt-approx f_lo f_des)
+              (op/lt-approx f_des f_hi))
+           (recur i_d (inc i_c)) ; Select next two `from` points to see if desired is between
+           (
+             (let [ratio (/ (- f_des f_lo) (- f_hi f_lo) )]
+               ; Use ratio to linearily interpolate between real and complex parts 
+               ; of each s parameter in nth matrix
+               )
+           ) ; Do interpolation
+            ))
+        ; () exit condition 
+        )
+      ))
+)
 
-(def interp-method-map
-  {:linear interp-one-freq-linear})
+(def -interp-method-map
+  {:linear -interp-to-s-linear
+  :cubic -interp-to-s-cubic
+   })
 
 (defn interpolate
   "Interpolate the response of the `rf-cljs/network` passed as `from` 
-   onto the frequencies defined by `(:freq onto)`"
+   onto the frequencies defined by `(:freq onto)`
+   "
   ([net desired-freq] (interpolate net desired-freq :linear))
   ([net desired-freq method]
    (let ([onto_f0 (first desired-freq)
@@ -24,10 +53,9 @@
          (if (and
               (op/lt-approx onto_f0 from_f0)
               (op/lt-approx from_fn onto_f0))
-           ({:s (for [f desired-freq]
-                  ((:linear interp-method-map) net f))
+           ({:s ((method -interp-method-map) net desired-freq)
              :freq desired-freq
              :z0 (:z0 net)})
            (assert false (str "Could not map `from` network with frequency range "
-                              from_f0 " to " from_fn " onto `onto` map with frequency range "
+                              from_f0 " to " from_fn " onto `onto` frequency range "
                               onto_f0 " to " onto_fn " .")))))))
