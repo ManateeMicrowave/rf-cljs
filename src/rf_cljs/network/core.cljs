8149d3ce4b77d0791651c076263a2d0789d28881
Eric Valentino <evalentino@mail.usf.edu>
2021-03-31 01:05:18 -0400

    filling out rest of linear interpolation

--- src/rf_cljs/network/core.cljs
+++ src/rf_cljs/network/core.cljs
@@ -1,44 +1,53 @@
 (ns rf-cljs.network.core
   "Collection of functions which operate on network maps"
   (:refer-clojure :exclude [+ - * /])
-  (:require [rf-cljs.network.params :as params]
-            [rf-cljs.math.matrix :as mat]
-            [rf-cljs.math.operations :as op]
-            [rf-cljs.math.operations :refer [abs square + - * / sqrt]]))
+  (:require
+   [goog.string :as gstring]
+   [goog.string.format]
+   [clojure.string :as str]
+   [rf-cljs.network.params :as params]
+   [rf-cljs.math.matrix :as mat]
+   [rf-cljs.math.operations :as op]
+   [rf-cljs.math.operations :refer [abs square + - * / sqrt]]))
 
 (defn -interp-to-s-linear
   [net desired-freq] ; TODO implement 
   (let [s (:s net)
         freq (:freq net)
         n-desired-freqs (count desired-freq)]
-    (loop [i_d 0 ;Index of the desired frequency that we are going to interpolate on
-           i_c 0 ;Index of the `from` frequency range. 
-           ]
-      (if-not (= (inc i_d))
-        (let [f_des (nth i_d desired-freq)
-              f_lo (nth i_c freq)
-              f_hi (nth (inc i_c) freq)]
-          (if-not 
-           (and
-              (op/lt-approx f_lo f_des)
-              (op/lt-approx f_des f_hi))
-           (recur i_d (inc i_c)) ; Select next two `from` points to see if desired is between
-           (
-             (let [ratio (/ (- f_des f_lo) (- f_hi f_lo) )]
+    (loop [i-d 0 ;Index of the desired frequency that we are going to interpolate on
+           i-c 0 ;Index of the `from` frequency range. 
+           s-output []]
+      (if-not (= i-d n-desired-freqs)
+      ; If we haven't gone past the number of desired frequencies
+        (let [f-des (nth desired-freq i-d)
+              f-lo (nth freq i-c)
+              f-hi (nth  freq (inc i-c))]
+          (if-not (and (op/lt-approx f-lo f-des) (op/lt-approx f-des f-hi))
+          ; If `f-des` isn't in between `f-lo` and `f-hi` 
+            (recur i-d (inc i-c) s-output)
+            ; Select next two `from` points to see if desired is between
+            ((let [ratio (/ (- f-des f-lo) (- f-hi f-lo))
+                   s-vec-lo (params/-destructure-two-port (mat/idx s i-c))
+                   s-vec-hi (params/-destructure-two-port (mat/idx s (inc i-c)))
+                   s-interp (mat/reshape (mat/matrix  (apply #(+ (* ratio %1) (* (- 1 ratio) %2)) (map vector s-vec-lo s-vec-hi))) [2 2])]
+               (recur (inc i-d) i-c (conj s-output s-interp))
                ; Use ratio to linearily interpolate between real and complex parts 
                ; of each s parameter in nth matrix
-               )
-           ) ; Do interpolation
-            ))
-        ; () exit condition 
-        )
-      ))
-)
+               ))))
+        {:s s-output
+         :freq desired-freq
+         :z0 (:z0 net)}))))
+         ; Exit condition, return hopefully completed interpolated net map
+
+(defn -interp-to-s-cubic
+  [net desired-freq] ; TODO implement 
+  (println (str net desired-freq))
+  nil)
 
 (def -interp-method-map
   {:linear -interp-to-s-linear
-  :cubic -interp-to-s-cubic
-   })
+   :cubic -interp-to-s-cubic})
 
 (defn interpolate
   "Interpolate the response of the `rf-cljs/network` passed as `from` 
